73d4615984ea40cb34c6b823fe5ec44b07f70848
Scott Dixon <scott@howrefreshing.com.au>
2017-11-17 12:38:53 +1000

    Express POST

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 const port = 3000
 
-let students = ['Scott', 'Simon']
+let students = ['Scott', 'Simon'];
 
-function handleRequest(request, response) {
+app.use(bodyParser.json())
 
-  if (request.url === '/students') {
-    response.writeHead(200, {
-      'Content-Type': 'application/json'
-    })
-    response.end(JSON.stringify(students))
-  }
+app.get('/students', function (req, res) {
+  res.send(students)
+})
 
-}
+app.post('/students', function (req, res) {
+  let student = req.body.name;
+  students.push(student);
+  res.send(student);
+})
 
-const server = http.createServer(handleRequest)
+app.use(function(req, res){
+  console.log('404')
+  res.sendStatus(404);
+})
 
-server.listen(port)
+app.listen(port)
