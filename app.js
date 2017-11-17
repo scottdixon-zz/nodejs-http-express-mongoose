31e666694b882b415aa68d1b48faa836bdafaf7a
Scott Dixon <scott@howrefreshing.com.au>
2017-11-17 13:25:42 +1000

    Mongoose - persist students

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const db = mongoose.createConnection('mongodb://localhost/students');
+const Schema = mongoose.Schema;
+const studentSchema = Schema({name: String});
+mongoose.Promise = Promise;
 
-let students = ['Scott', 'Simon'];
+student = db.model('Student', studentSchema);
 
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('view engine', 'pug')
 
 app.get('/students', function (req, res) {
-  res.render('students', { students: students })
+  student.find().then(function(students) {
+    res.render('students', { students: students })
+  }).catch(function(err){
+    next(err)
+  })
 })
 
 app.post('/students', function (req, res) {
-  let student = req.body.student;
-  students.push(student);
-  res.redirect('/students');
+  student.create({name: req.body.student}).then(function(){
+    res.redirect('/students');
+  }).catch(function(err){
+    next(err);
+  })
 })
 
 app.use(function(req, res){
