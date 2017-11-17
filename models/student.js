003f10bafd88fc2248ce72d214f6893cff96edad
Scott Dixon <scott@howrefreshing.com.au>
2017-11-17 13:46:11 +1000

    DB refactor

--- models/student.js
+++ models/student.js
@@ -1,9 +1,5 @@
-const mongoose = require('mongoose')
-const db = mongoose.createConnection('mongodb://localhost/students');
-const Schema = mongoose.Schema
-const studentSchema = Schema({name: String})
-mongoose.Promise = Promise
-
+const { mongoose, db, schema } = require('../db')
+const studentSchema = schema({name: String})
 student = db.model('Student', studentSchema)
 
 module.exports = student
